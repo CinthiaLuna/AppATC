7866c6178085bd2bf61f0f8552434a251554e24c
SelMoLu <44171203+CinthiaLuna@users.noreply.github.com>
2020-02-06 12:23:45 -0600

    Add files via upload

---
+++ AppATC/src/main/java/com/crisver/appatc/AppAtcApplication.java
@@ -0,0 +1,13 @@
+package com.crisver.appatc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAtcApplication.class, args);
+	}
+
+}
